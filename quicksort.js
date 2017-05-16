f17bc54043ba576017589fd4b2796a6751114cae
Eduardo Santiago <Eduardo Santiago>
2017-05-15 22:43:30 -0300

    criação e exibição do array com números aleatórios

--- quicksort.js
+++ quicksort.js
@@ -1,19 +1,34 @@
-
-	// $(document).ready(function(){
-	// 	alert('teste')
-	// });
 var arrayGerado = new Array();
-$('#btn-gerar').click(()=>{
-	let tamanhoArray = parseInt(($('#qtdArray').val()));
-	for (let i = 0; i <= tamanhoArray; i++) {
-		arrayGerado.push(randomInt(-tamanhoArray,tamanhoArray));
-	}
-	console.log(arrayGerado)
+
+$('#btn-gerar').click(() => {
+    let tamanhoArray = parseInt(($('#qtdArray').val()));
+    if(tamanhoArray){
+	    for (let i = 0; i <= tamanhoArray; i++) {
+	        arrayGerado.push(randomInt(-tamanhoArray, tamanhoArray));
+	    }
+	    construirInterfaceArray(arrayGerado);
+	    console.log(arrayGerado);
+    }
+    else{
+    	$("#mensagem").html('Primeiro insira o tamanho do array!');
+    }
 });
 
-function randomInt(min,max)
-{
-    return Math.floor(Math.random()*(max-min+1)+min);
+$('#btn-ordenar').click(() => {
+	alert('Ainda falta inplementar')
+});
+
+
+function randomInt(min, max) {
+    return Math.floor(Math.random() * (max - min + 1) + min);
 }
 
-// function()
+function construirInterfaceArray(array) {
+	arrayGerado = new Array();
+	$("#arrayText").empty();
+	$("#mensagem").empty();
+
+    for (let i = 0; i <= array.length-1; i++) {
+        $("#arrayText").append("<span style='width: 10px; color:blue;text-align: justify;'> "+array[i]+" </span>");
+    }
+}
